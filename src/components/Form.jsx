5a7c130c552df189c0bd5847d5686003d018f4af
Anton Stambler <anstambler@gmail.com>
2025-11-14 10:54:57 +0200

    Add state management for city input in Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,22 @@
+import {useState} from "react";
 
 const Form = ({getWeather}) => {
+    const [city, setCity] = useState('');
 
 
     const handleClickSubmit = e => {
         e.preventDefault(); // otmenit defoltnoe povedenie
-        const city = e.currentTarget.city.value.trim();
         getWeather(city)
+        setCity('');
     }
 
     return (
         <form onSubmit={handleClickSubmit}>
-            <input type={'text'} name={'city'}/>
+            <input
+                type={'text'}
+                value={city}
+                onChange={e => setCity(e.target.value)}
+            />
             <button type={'submit'}>Get Weather</button>
         </form>
     )
